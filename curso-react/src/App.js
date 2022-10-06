01aadc77c3285e83716d6c3c0b1eff1456796b0a
Bianca Amarante Magalhães <bianca.amarante@venturus.org.br>
2022-10-05 21:50:12 -0300

    [development] dev: todoList project

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import AnotherComponent from './components/AnotherComponent';
+import Container from './components/Container';
 import FirstComponent from './components/FirstComponent';
+import Fragment from './components/Fragment';
 import Hooks from './components/Hooks';
 import Images from './components/Images';
 import List from './components/List';
@@ -16,6 +18,10 @@ function App() {
       <Hooks />
       <List />
       <RenderCond x = {10} y = {20}/>
+      <Fragment/>
+      <Container>
+        <h1>Este é filho container!</h1>
+      </Container>
     </div>
   );
 }
